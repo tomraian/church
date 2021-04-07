e15dcaa55ec7689eeece312b0102ac2b922116a1
tomraian <duyhoangdinh281@gmail.com>
2021-04-08 00:30:31 +0700

    upload-8/04/2021

--- js/main.js
+++ js/main.js
@@ -140,6 +140,24 @@ $(document).ready(function() {
     });
 });
 
+
+$('.testimonials__carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    resize: true
+
+  });
+
+
+
+
+
+
+
+
+
 // calendar
 
 function calendar(){
